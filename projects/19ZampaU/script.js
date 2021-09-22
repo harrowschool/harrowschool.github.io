5c7bf09f4e6c10396e83580efc6810097195dbc9
Peter Davison-Reiber <pdavisonreiber@users.noreply.github.com>
2021-09-22 18:01:28 +0100

    Added project

---
+++ projects/19ZampaU/script.js
@@ -0,0 +1,129 @@
+function question1() {
+  var value = document.getElementById("quest1").value;
+
+  if (value == '5') 
+    {
+    alert("Correct, well done!")
+    }
+  else if (value <10 ) 
+    {
+    alert("Too low, try again")
+    }
+
+  else 
+    {
+    alert("Too high, try again")
+    }
+}
+
+function question2() {
+  var value = document.getElementById("quest2").value;
+
+  if (value == 'semolina' || value == 'durum' ) 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+}
+  
+function question3() {
+  var value = document.getElementById("quest3").value;
+
+  if (value == 'Tomato' || value == 'tomato' || value == 'tomato sauce' ) 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+
+
+}function question4() {
+  var value = document.getElementById("quest4").value;
+
+  if (value == 'Barilla' || value == 'Barila' || value == 'barilla') 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+}
+
+function question5() {
+  var value = document.getElementById("quest5").value;
+
+  if (value == 'far fall lay' || value == 'far-fall-lay' ) 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+}
+
+function question6() {
+  var value = document.getElementById("quest6").value;
+
+  if (value == 'China' || value == 'china' ) 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+}
+
+function question7() {
+  var value = document.getElementById("quest7").value;
+
+  if (value == '7' || value == '8' ) 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+}
+
+function question8() {
+  var value = document.getElementById("quest8").value;
+
+  if (value == 'Tagliatelle' || value == 'tagliatelle' ) 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+}
+
+function question9() {
+  var value = document.getElementById("quest9").value;
+
+  if (value == 'tiramisu' || value == 'Tiramisu' ) 
+    {
+    alert("Correct, well done!")
+    }
+
+  else 
+    {
+    alert("Incorrect, try again")
+    }
+}
